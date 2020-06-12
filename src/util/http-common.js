4b73fe2fb8a97d514761122d0093f263300054d2
wonthechan <packerstorm@gmail.com>
2020-06-12 17:51:13 +0900

    관심지역 관리 기능(추가/삭제) 컴포넌트 완료. (경로: @/views/interestarea/manage/index.vue

--- src/util/http-common.js
+++ src/util/http-common.js
@@ -4,6 +4,6 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'http://localhost:9999',
   headers: {
-    'Content-type': 'applicatoin/json',
+    // 'Content-type': 'applicatoin/json',
   },
 })
--- src/views/interestarea/manage/index.vue
+++ src/views/interestarea/manage/index.vue
@@ -22,11 +22,11 @@
                   <option value="0">선택</option>
                   <option v-for="(gugun, index) in guguns" :key="'item_' + index" :value="gugun.gucode">{{ gugun.gu }}</option>
                 </select>
-                <select class="form-control" id="dong" v-model="currentDongCode">
+                <select class="form-control" id="dong" v-model="currentDong">
                     <option value="0">선택</option>
-                    <option v-for="(dong, index) in dongs" :key="'item_' + index" :value="dong.dongcode">{{ dong.dong }}</option>
+                    <option v-for="(dong, index) in dongs" :key="'item_' + index" :value="{ dongCode: dong.dongcode, dong: dong.dong }">{{ dong.dong }}</option>
                 </select>
-                    <button class="btn btn-primary" id="registerArea">관심지역 등록</button>
+                    <button class="btn btn-primary" id="registerArea" @click="registerArea()">관심지역 등록</button>
                   </td>
                 </tr>
               </table>
@@ -56,6 +56,7 @@
 </template>
 <script>
 import http from '@/util/http-common.js'
+
 export default {
   name: 'interestareaManage',
   components: {
@@ -66,7 +67,7 @@ export default {
       userAreas: [],
       currentSidoCode: '0',
       currentGuCode: '0',
-      currentDongCode: '0',
+      currentDong: '0',
       sidos: [],
       guguns: [],
       dongs: [],
@@ -74,22 +75,9 @@ export default {
   },
   created() {
     // 사용자 등록 관심 지역 받아오기 (ssafy 테스트 계정)
-    http.get('/areas/interest/' + this.uid)
-      .then(({ data }) => {
-        this.userAreas = data
-      })
+    this.updateUserAreas()
     // 시도코드 데이터 받아오기
     http.get('/areas/sido')
-      .then(({ data }) => {
-        this.sidos = data
-      })
-    // 구군코드 데이터 받아오기
-    http.get('/areas/gugun')
-      .then(({ data }) => {
-        this.sidos = data
-      })
-    // 동코드 데이터 받아오기
-    http.get('/areas/dong')
       .then(({ data }) => {
         this.sidos = data
       })
@@ -103,7 +91,7 @@ export default {
           this.guguns = data
         })
       this.currentGuCode = '0'
-      this.currentDongCode = '0'
+      this.currentDong = '0'
     },
     // 구군 선택 시 동 데이터 받아오기
     getDongList() {
@@ -112,20 +100,51 @@ export default {
         .then(({ data }) => {
           this.dongs = data
         })
-      this.currentDongCode = '0'
+      this.currentDong = '0'
     },
     // 관심 지역 삭제 요청
     deleteArea(dongCode) {
-      http.delete({ url: '/areas/interest/', data: { uid: this.uid, dongcode: dongCode } })
-        .then((data) => {
+      http.delete('/areas/interest', { data: { uid: this.uid, dongcode: dongCode } })
+        .then(({ data }) => {
+          console.log(data)
           if (data === 1) {
             alert('삭제되었습니다.')
+            this.updateUserAreas()
           } else {
             alert('삭제 중 문제가 발생하였습니다.')
           }
         })
-        .catch(() => {
+        .catch((data) => {
+          console.log(data)
           alert('삭제 중 문제가 발생하였습니다.')
+        })
+    },
+    // 관심 지역 받아오기
+    updateUserAreas() {
+      http.get('/areas/interest/' + this.uid)
+        .then(({ data }) => {
+          this.userAreas = data
+        })
+    },
+    // 관심 지역 등록하기
+    registerArea() {
+      if (this.currentDong === '0') { // 지역을 선택하지 않은 경우 종료
+        alert('지역을 선택해주세요!')
+        return
+      }
+      http.post('/areas/interest', { uid: this.uid, dongcode: this.currentDong.dongCode, dong: this.currentDong.dong })
+        .then(({ data }) => {
+          console.log(data)
+          if (data === 1) {
+            alert('등록되었습니다.')
+            this.updateUserAreas()
+          } else {
+            alert('등록 중 문제가 발생하였습니다.')
+          }
+        })
+        .catch((data) => {
+          console.log(data)
+          alert('등록 중 문제가 발생하였습니다.')
         })
     },
   },
